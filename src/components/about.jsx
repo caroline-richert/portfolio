a29247a8b429da9f4b5a9f577c94ad3a35e1ab25
Caroline Richert <caroline.richert92@gmail.com>
2023-01-12 13:48:46 +0100

    Updated Styles and added markdown

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,20 +1,19 @@
+import PortfolioImg from '../assets/IMG_7943.jpg'
+
 function About(){
     return(
-        <div className="row  d-flex justify-content-evenly portfolio--about main--content">
-            <section className="name-section col-7">
+        <main className="main--content">
+            <div className="portfolio--about">
+            <section className="name-section">
                 <h1 className="headline text-muted">Caroline Richert</h1>
                 <h6 className="subheadline">From Physician to Front End</h6>
                 <div className="light mt-3" role="alert">
-                Lorem ipsum dolor sit amet,
-                consetetur sadipscing elitr,
-                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed diam voluptua. At vero eos et accusam et justo
-                duo dolores et ea rebum. Stet clita kasd gubergren,
-                no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+                
                 </div>
             </section>
-            <img src="https://picsum.photos/300" className="portfolio--image"/>
-        </div>
+            <img src={PortfolioImg} className="portfolio--image"/>
+            </div>
+        </main>
         
 
     )
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
+import PortfolioImg from './assets/IMG_7943.jpg'
+
 function Index(){
     return(
         <div className="index-page main--content">
-            <h1 className="text-muted">Hi, I am Caro. A Front End Developer and former Physician.</h1>
-            <img src="https://picsum.photos/300" className="portfolio--image"/>
+            <h1 className="text-muted">Hi, I am Caro. A Front End Developer based in Germany.</h1>
+            <img src={PortfolioImg} className="portfolio--image"/>
         </div>
     )
 }
